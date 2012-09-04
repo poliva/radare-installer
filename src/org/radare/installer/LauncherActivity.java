b9bfb5e6173b538685b95a60584709a5034d1622
Pau Oliva <pau@eslack.org>
2012-09-04 20:55:17 +0200

    Add a new launcher activity

---
+++ src/org/radare/installer/LauncherActivity.java
@@ -0,0 +1,32 @@
+/*
+radare2 installer for Android
+(c) 2012 Pau Oliva Fora <pof[at]eslack[dot]org>
+*/
+package org.radare.installer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+
+
+public class LauncherActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		// we don't need a layout for this activity as we finish() it right after the intent has started
+		//setContentView(R.layout.launcher);
+
+		try {
+			Intent i = new Intent("jackpal.androidterm.RUN_SCRIPT");
+			i.addCategory(Intent.CATEGORY_DEFAULT);
+			i.putExtra("jackpal.androidterm.iInitialCommand", "export PATH=$PATH:/data/data/org.radare.installer/radare2/bin/ ; radare2 /system/bin/ls");
+			startActivity(i);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		finish();
+	}
+}
