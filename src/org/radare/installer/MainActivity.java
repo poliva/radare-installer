3324e515d2499f11459a08be2553a6dda12a01af
Pau Oliva <pau@eslack.org>
2012-09-15 16:19:22 +0200

    move SendNotification into Utils

--- src/org/radare/installer/MainActivity.java
+++ src/org/radare/installer/MainActivity.java
@@ -38,10 +38,6 @@ import android.os.Build;
 import android.content.Intent;
 import android.net.Uri;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -86,7 +82,7 @@ public class MainActivity extends Activity {
 						boolean update = mUtils.UpdateCheck(url);
 						if (update) {
 							output ("New radare2 " + version + " version available!\n");
-							SendNotification("Radare2 update", "New radare2 " + version + " version available!\n");
+							mUtils.SendNotification("Radare2 update", "New radare2 " + version + " version available!\n");
 						}
 					}
 				}
@@ -111,26 +107,6 @@ public class MainActivity extends Activity {
 		return false;
 	}
 
-	private void SendNotification(String title, String message) {
-		NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
-		int icon = R.drawable.icon;
-		CharSequence tickerText = title;
-		long when = System.currentTimeMillis();
-		Notification notification = new Notification( icon, tickerText, when);
-		notification.flags |= Notification.FLAG_AUTO_CANCEL;
-
-		Context context = getApplicationContext();
-		CharSequence contentTitle = title;
-		CharSequence contentText = message;
-		Intent notificationIntent = new Intent(this, MainActivity.class);
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-
-		notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
-
-		//  Send the notification
-		nm.notify( 1, notification );
-	}
 
 
 	private OnClickListener onLocalRunButtonClick = new OnClickListener() {
--- src/org/radare/installer/Utils.java
+++ src/org/radare/installer/Utils.java
@@ -28,7 +28,10 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-
+import android.content.Intent;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 
 public class Utils {
 
@@ -119,5 +122,27 @@ public class Utils {
 		}
 		return update;
 	}
+
+	public void SendNotification(String title, String message) {
+		NotificationManager nm = (NotificationManager) mContext.getSystemService(mContext.NOTIFICATION_SERVICE);
+
+		int icon = R.drawable.icon;
+		CharSequence tickerText = title;
+		long when = System.currentTimeMillis();
+		Notification notification = new Notification( icon, tickerText, when);
+		notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+		Context context = mContext.getApplicationContext();
+		CharSequence contentTitle = title;
+		CharSequence contentText = message;
+		Intent notificationIntent = new Intent(mContext, MainActivity.class);
+		PendingIntent contentIntent = PendingIntent.getActivity(mContext, 0, notificationIntent, 0);
+
+		notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+
+		//  Send the notification
+		nm.notify( 1, notification );
+	}
+
 
 }
